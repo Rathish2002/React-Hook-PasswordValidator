851a2c968da04e109a9639fa559edae938c41263
Rathish2002 <160592269+Rathish2002@users.noreply.github.com>
2024-05-08 13:47:46 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+// Write your code here
+import {useState} from 'react'
+
+import {
+  BgContainer,
+  BoxContainer,
+  Heading,
+  Paragraph,
+  InputText,
+  ErrPara,
+} from './styledComponents'
+
+const PasswordValidator = () => {
+  const [isPasswordValidator, setPasswordValidator] = useState(false)
+
+  const checkStatus = isPasswordValidator.length >= 8
+  const errMsg = checkStatus
+    ? ''
+    : 'Your password must be at least 8 charecters'
+
+  const changeText = event => setPasswordValidator(prevState => !prevState)
+
+  return (
+    <BgContainer>
+      <BoxContainer>
+        <Heading>Password Validator</Heading>
+        <Paragraph>Check how strong and secure is your password</Paragraph>
+        <InputText type="password" />
+        <ErrPara>{errMsg}</ErrPara>
+      </BoxContainer>
+    </BgContainer>
+  )
+}
+
+export default PasswordValidator
